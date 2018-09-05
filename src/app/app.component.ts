10e0deb62cae3f71485e99a4f281d0059f2b9893
Jargil Santos <urban-fox@users.noreply.github.com>
2018-09-05 14:56:28 +1200

    Very rough html template to show objects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Semester } from './models/semester';
+import { SemesterService } from './semester.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'TaskMaster';
+export class AppComponent implements OnInit{
+  title: string = 'TaskMaster';
+  semester: Semester;
+
+  constructor(private semesterService: SemesterService){}
+
+  ngOnInit() {
+    this.semester = this.semesterService.getSemester();
+    console.log(this.semester);
+  }    
   
 }
